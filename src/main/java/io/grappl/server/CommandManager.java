4208b07ba36de72ad5e5bdc71c727769d49e433f
VaiTon86 <eyadlorenzo@gmail.com>
2016-06-03 12:39:47 +0200

    Added exception handling for command "inspect" and created the method error()

--- src/main/java/io/grappl/server/CommandManager.java
+++ src/main/java/io/grappl/server/CommandManager.java
@@ -19,36 +19,35 @@ public class CommandManager {
 
         final String commandName = parts[0];
 
-        if(commandName.equalsIgnoreCase("inspect")) {
-            final int port = Integer.parseInt(parts[1]);
+        if (commandName.equalsIgnoreCase("inspect")) {
+            try {
+                final int port = Integer.parseInt(parts[1]);
 
-            Host host = relay.getHostByPort(port);
-            Log.log(host.getHostSnapshot().toJson());
-        }
 
-        else if(commandName.equalsIgnoreCase("close")) {
+                Host host = relay.getHostByPort(port);
+                Log.log(host.getHostSnapshot().toJson());
+            } catch (IndexOutOfBoundsException e) {
+                error("inspect", "Command use: inspect <port>");
+            } catch (NullPointerException e) {
+                Log.log("Specified port isn't used");
+            }
+        } else if (commandName.equalsIgnoreCase("close")) {
             final int port = Integer.parseInt(parts[1]);
 
             Host host = relay.getHostByPort(port);
             host.closeHost();
-        }
-
-        else if(commandName.equalsIgnoreCase("hosts")) {
+        } else if (commandName.equalsIgnoreCase("hosts")) {
             Log.log(relay.getHostList().size() + " hosts open");
         }
 
         /* Begin imported old code */
-        else if(commandName.equalsIgnoreCase("hostlist")) {
+        else if (commandName.equalsIgnoreCase("hostlist")) {
             List<Host> hosts = relay.getHostList();
 
             String output = hosts.size() + " host(s): ";
 
-            for (int i = 0; i < hosts.size(); i++) {
-                if(i != 0) {
-                    output += " - ";
-                }
-                Host host = hosts.get(i);
-
+            for (Host host : hosts) {
+                output += " - ";
                 output += host.getControlSocket().getInetAddress().toString() + ":" + host.getPort();
             }
 
@@ -56,17 +55,17 @@ public class CommandManager {
         }
         /* End old code */
 
-        else if(commandName.equalsIgnoreCase("coreconnect")) {
+        else if (commandName.equalsIgnoreCase("coreconnect")) {
             Application.application.connectToCore();
-        }
-
-        else if(commandName.equalsIgnoreCase("quit")) {
+        } else if (commandName.equalsIgnoreCase("quit")) {
             System.exit(0);
-        }
-
-        else {
+        } else {
             Log.log("Command not found");
         }
+    }
+
+    private void error(String command, String motivation) {
+        Log.log(String.format("ERROR in command %s: %s", command, motivation));
     }
 
     public void startCommandThread() {
@@ -75,7 +74,7 @@ public class CommandManager {
             public void run() {
 
                 Scanner scanner = new Scanner(System.in);
-                while(true) {
+                while (true) {
                     String line = scanner.nextLine();
                     executeCommand(line);
                 }
